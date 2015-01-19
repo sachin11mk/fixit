ad4a5cc2b8b99c6d33e2d1b48c181773c40cf042
sachin <sachin.karambalkar@coriolis.co.in>
2015-01-19 21:06:49 +0530

    changed DB and mail settings for test branch

--- fixit/settings.py
+++ fixit/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'fixit.db'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fixit',
+        'NAME': 'test_fixit',
         'USER': 'colama',
         'PASSWORD': 'coriolis',
         'HOST': 'localhost',
